6e5c58be716d2b35bc8c08912df3abc9f152241e
WilliamParoff <paroffwilliam@gmail.com>
2023-11-21 08:30:54 -0600

    added mana to character class

--- src/main/java/com/csproject/character/Character.java
+++ src/main/java/com/csproject/character/Character.java
@@ -5,7 +5,10 @@ public abstract class Character {
     private static final double BASE_HP = 100;
     private static final double HP_PER_LEVEL = 10;
 
+    private static final double MANA_PER_INTELLIGENCE = 25;
+
     private double hp;
+    private double mana;
     private int level;
 
     protected final CharacterAttribute strength;
@@ -14,11 +17,12 @@ public abstract class Character {
 
     protected Character(int level, int strength, int intelligence, int agility) {
         this.level = level;
-        this.hp = getMaxHp();
-
         this.strength = new CharacterAttribute("strength", strength);
         this.intelligence = new CharacterAttribute("intelligence", intelligence);
         this.agility = new CharacterAttribute("agility", agility);
+
+        this.hp = getMaxHp();
+        this.mana = getMaxMana();
     }
 
     public double getHp() {
@@ -40,6 +44,15 @@ public abstract class Character {
     private double getMaxHp() {
         return BASE_HP + (HP_PER_LEVEL * (this.level - 1));
     }
+
+    private double getMana() {
+        return this.mana;
+    }
+
+    private double getMaxMana() {
+        return MANA_PER_INTELLIGENCE * intelligence.getValue();
+    }
+
 
     public boolean isDead() {
         return this.hp < 0.0;
