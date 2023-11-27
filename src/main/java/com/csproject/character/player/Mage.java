6f4b7d2940be6ed5c0c7f634c4fd019c71b3ad64
JacobBruen <151602291+JacobBruen@users.noreply.github.com>
2023-11-26 21:00:08 -0600

    Update Mage.java
    Created methods for abilities for the mage class.

--- src/main/java/com/csproject/character/player/Mage.java
+++ src/main/java/com/csproject/character/player/Mage.java
@@ -7,7 +7,19 @@ public class Mage extends Player {
     public Mage(String name, int level, int strength, int intelligence, int agility) {
         super(name, level, strength, intelligence, agility);
     }
-
+    public void castFireball() {
+        int damage = intelligence * 2;
+    }
+    public void summonIceBarrier() {
+        int barrierStrength = agility * 3;
+    }
+    public void teleport() {
+        if (agility >= 20) {
+            System.out.println(name + " teleports to a new location!");
+        } else {
+            System.out.println(name + " attempts to teleport but fails due to low agility.");
+        }
+    }
     @Override
     public CombatAction combat() {
         return null;
