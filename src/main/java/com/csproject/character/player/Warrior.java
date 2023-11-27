dd8e049dd4350aaac138b91378d358490480cafd
JacobBruen <151602291+JacobBruen@users.noreply.github.com>
2023-11-26 21:05:19 -0600

    Update Warrior.java

--- src/main/java/com/csproject/character/player/Warrior.java
+++ src/main/java/com/csproject/character/player/Warrior.java
@@ -7,7 +7,22 @@ public class Warrior extends Player {
     public Warrior(String name, int level, int strength, int intelligence, int agility) {
         super(name, level, strength, intelligence, agility);
     }
+     public void attack() {
+        int attackDamage = strength * 2;
+    }
 
+    public void shieldBlock() {
+        int blockStrength = strength + agility;
+    }
+
+    public void berserkerRage() {
+        if (strength >= 15 && level >= 10) {
+            System.out.println(name + " enters a berserker rage, gaining increased strength!");
+        } else {
+            System.out.println(name + " attempts to enter a berserker rage but fails.");
+        }
+    }
+    
     @Override
     public CombatAction combat() {
         return null;
