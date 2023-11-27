dfc35f1f8f0bb0c2f8bc441a00d35f21faaed41d
JacobBruen <151602291+JacobBruen@users.noreply.github.com>
2023-11-26 21:03:03 -0600

    Update Archer.java
    Created methods for the archer class for abilities.

--- src/main/java/com/csproject/character/player/Archer.java
+++ src/main/java/com/csproject/character/player/Archer.java
@@ -7,6 +7,26 @@ public class Archer extends Player {
     public Archer(String name, int level, int strength, int intelligence, int agility) {
         super(name, level, strength, intelligence, agility);
     }
+    
+    public void shootArrow() {
+        int arrowDamage = strength + agility;
+    }
+    
+    public void precisionShot() {
+        if (intelligence >= 15) {
+            System.out.println(name + " performs a precision shot!");
+        } else {
+            System.out.println(name + "'s precision shot fails due to low intelligence.");
+        }
+    }
+    
+    public void evasiveManeuver() {
+        if (agility >= 20) {
+            System.out.println(name + " performs an evasive maneuver!");
+        } else {
+            System.out.println(name + " attempts an evasive maneuver but fails due to low agility.");
+        }
+    }
 
     @Override
     public CombatAction combat() {
