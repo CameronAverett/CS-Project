504d8e402972cf1fe7c88c22d2c966de9e7c18e5
JacobBruen <151602291+JacobBruen@users.noreply.github.com>
2023-11-27 12:05:40 -0600

    Update Mage.java

--- src/main/java/com/csproject/character/player/Mage.java
+++ src/main/java/com/csproject/character/player/Mage.java
@@ -9,9 +9,11 @@ public class Mage extends Player {
     }
     public void castFireball() {
         int damage = getAgility() * 2;
+         return new CombatAction("Cast Fireball", attackDamage, 0.5);
     }
     public void summonIceBarrier() {
         int barrierStrength = getAgility() * 3;
+         return new CombatAction("Summon Ice Barrier", blockStrength, 0.5);
     }
     public void teleport() {
         if (getAgility() >= 20) {
@@ -19,9 +21,28 @@ public class Mage extends Player {
         } else {
             System.out.println(name + " attempts to teleport but fails due to low agility.");
         }
+        return new CombatAction("Teleport", 0.0, getAgility() >= 20);
     }
+    
     @Override
     public CombatAction combat() {
-        return null;
+        GameResponse response = new GameResponse(Game.getInstance().getIn(), "Which move do you want to use? ");
+        response.setResponses(List.of("Cast Fireball", "Summon Ice Barrier", "Teleport"));
+
+        response.displayResponses("\nAvailable Moves");
+        String responseValue = response.getResponse();
+
+        switch (responseValue) {
+            case "Cast Fireball" -> {
+                return castFireball();
+            }
+            case "Summon Ice Barrier" -> {
+                return summonIceBarrier();
+            }
+            case "Teleport" -> {
+                return teleport();
+            }
+            default -> throw new CombatResponseException(responseValue);
+        }
     }
 }
